23683b8b4a147e6e68f104b967a3b22f5b69593c
aniskovichav <aniskovichav@gmail.com>
2024-09-10 21:46:16 +0300

    update

---
+++ src/main/java/org/example/PersonService.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import org.apache.commons.lang.StringUtils;
+import org.example.entity.Person;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+@Path("/persons")
+public class PersonService {
+
+    private static List<Person> data;
+    static {
+        data = new ArrayList<>();
+        Person p = new Person();
+        p.setId(100);
+        p.setName("John");
+
+        Person p1 = new Person();
+        p1.setId(200);
+        p1.setName("Bob");
+
+        Person p2 = new Person();
+        p2.setId(300);
+        p2.setName("Mike");
+
+        data.addAll(Arrays.asList(p, p1, p2));
+    }
+
+    @GET
+    @Path("/test")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    public Response test() {
+       if(data.size()<1){
+           return Response.status(Response.Status.NO_CONTENT).entity("No persons found").build();
+       }
+        int index = new Random().nextInt(data.size());
+       return Response.ok(data.get(index)).build();
+    }
+}
---
+++ src/main/java/org/example/entity/Person.java
@@ -0,0 +1,13 @@
+package org.example.entity;
+
+import lombok.Data;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+@XmlRootElement
+@Data
+public class Person {
+    private int id;
+    private String name;
+}
