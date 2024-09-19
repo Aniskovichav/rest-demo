e6bea8576466d0ff1cd91fdc83172519c4e15cab
aniskovichav <aniskovichav@gmail.com>
2024-09-19 21:36:06 +0300

    add entity Currency

--- src/main/java/org/example/entity/Currency.java
+++ src/main/java/org/example/entity/Currency.java
@@ -7,6 +7,7 @@ import lombok.ToString;
 import javax.persistence.*;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "Currency")
@@ -19,21 +20,22 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class Currency {
 
     @Id
+
     @Column(name="id", nullable=false, unique=true, length=11)
-    private int id;
+    @XmlAttribute private int id;
 
-    @Column(name = "numCode", length = 20)
-    private String numCode;
+    @Column(name = "NumCode", length = 20)
+    private String NumCode;
 
-    @Column(name = "charCode", length = 20)
-    private String charCode;
+    @Column(name = "CharCode", length = 20)
+    private String CharCode;
 
-    @Column(name="name", length=50)
-    private String name;
+    @Column(name="Name", length=50)
+    private String Name;
 
-    @Column(name="scale", length=20)
-    private String scale;
+    @Column(name="Scale", length=20)
+    private String Scale;
 
-    @Column(name = "rate", length = 20)
-    private String rate;
+    @Column(name = "Rate", length = 20)
+    private String Rate;
 }
